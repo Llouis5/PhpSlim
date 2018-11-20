f1dbd3239dd03f984af3e76f6f0ce3004a137ec5
Bongo Cat <37389322+Llouis5@users.noreply.github.com>
2018-11-20 10:55:23 +0100

    Changed friend ID

--- index.php
+++ index.php
@@ -4,6 +4,18 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 require 'vendor/autoload.php';
 
+class MyDB extends SQLite3 {
+    function __construct() {
+       $this->open('friends.db');
+    }
+ }
+
+ $db = new MyDB();
+ if(!$db) {
+    echo $db->lastErrorMsg();
+    exit();
+ } 
+ 
 $app = new \Slim\App;
 $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
     $name = $args['name'];
@@ -11,4 +23,19 @@ $app->get('/hello/{name}', function (Request $request, Response $response, array
 
     return $response;
 });
+$app->get(
+    '/friends/{id}',
+    function (Request $request, Response $response, array $args) use ($db) {
+		$id=$args['id'];
+        $sql = "select * from friend where id=$id";
+        $ret = $db->query($sql);
+        $friends = [];
+        while ($friend = $ret->fetchArray(SQLITE3_ASSOC)) {
+            $friends[] = $friend;
+        return $response->withJson($friends);
+       
+	}
+    }
+);
+
 $app->run();
